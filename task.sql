04b6afcb6d4e5e548da99fd42859b856991d92a2
Shershen Oleksandra <o_shershen@viyar.ua>
2026-04-13 15:27:05 +0300

    refactor(task.sql): change database naming

--- task.sql
+++ task.sql
@@ -4,53 +4,47 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
 
 CREATE TABLE Products (
-    ID INT NOT NULL AUTO_INCREMENT,
-    ProductName VARCHAR(50) NOT NULL,
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    ID INT PRIMARY KEY,
+    ProductName VARCHAR(50)
+);
 
 CREATE TABLE Warehouses (
-    ID INT NOT NULL AUTO_INCREMENT,
-    WarehouseName VARCHAR(50) NOT NULL,
-    WarehouseAddress VARCHAR(100) NOT NULL,
-    CountryID INT NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (CountryID)
-        REFERENCES Countries(ID)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    ID INT PRIMARY KEY,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
+);
 
-CREATE TABLE Inventory (
-    ProductID INT NOT NULL,
-    WarehouseID INT NOT NULL,
-    WarehouseAmount INT NOT NULL,
-    PRIMARY KEY (ProductID, WarehouseID),
-    FOREIGN KEY (ProductID)
-        REFERENCES Products(ID)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    FOREIGN KEY (WarehouseID)
-        REFERENCES Warehouses(ID)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE ProductInventory (
+    ID INT PRIMARY KEY,
+    ProductID INT,
+    WarehouseAmount INT,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
+);
 
-INSERT INTO Countries (Name) VALUES ('Country1');
-INSERT INTO Countries (Name) VALUES ('Country2');
+-- Populate test data
 
-INSERT INTO Products (ProductName) VALUES ('AwesomeProduct');
+-- Countries
+INSERT INTO Countries VALUES (1, 'Country1');
+INSERT INTO Countries VALUES (2, 'Country2');
 
-INSERT INTO Warehouses (WarehouseName, WarehouseAddress, CountryID) VALUES
-('Warehouse-1', 'City-1, Street-1', 1),
-('Warehouse-2', 'City-2, Street-2', 2);
+-- Products
+INSERT INTO Products VALUES (1, 'AwersomeProduct');
 
-INSERT INTO Inventory (ProductID, WarehouseID, WarehouseAmount) VALUES
-(1, 1, 2),
-(1, 2, 5);
+-- Warehouses
+INSERT INTO Warehouses VALUES
+(1, 'Warehouse-1', 'City-1, Street-1', 1),
+(2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+-- ProductInventory
+INSERT INTO ProductInventory VALUES
+(1, 1, 2, 1),
+(2, 1, 5, 2);
